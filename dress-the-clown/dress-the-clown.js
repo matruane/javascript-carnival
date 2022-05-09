4ccd672f99e7dd939267fab0545ffd1be0eb4e32
Mat Ruane <mat@ruane.net.nz>
2022-05-10 08:42:24 +1200

    Head changes on ArrowRight keydown

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -4,8 +4,21 @@
 
 const clownHead = document.querySelector('.dress-an-image.head')
 
+let headIndex = 0
+
 const changeClownHead = () => {
-  clownHead.src = './images/head1.png'
+  headIndex >= 5 ? (headIndex = 0) : headIndex++
+
+  clownHead.src = `./images/head${headIndex}.png`
 }
 
 changeClownHead()
+
+document.addEventListener('keydown', (e) => {
+  switch (e.code) {
+    case 'ArrowRight':
+      changeClownHead()
+      console.log('ArrowRight press')
+      break
+  }
+})
