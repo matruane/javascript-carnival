1eba4ed9895f58b88ae7af054348edb71abefb5c
Mat Ruane <mat@ruane.net.nz>
2022-05-09 15:31:06 +1200

    Builds a representation of the board and gets a random cell

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,22 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+const rows = document.getElementsByTagName('tr')
+const rowColLength = rows.length
+let board = []
+
+// build representation of board
+for (let i = 0; i < rows.length; i++) {
+  board.push([...rows[i].cells])
+}
+
+const getRandomCell = () => {
+  return board[createRandomIndex(rowColLength)][createRandomIndex(rowColLength)]
+}
+
+const createRandomIndex = (length) => {
+  return Math.floor(Math.random() * length)
+}
+
+console.log((getRandomCell().innerText = 'blah'))
