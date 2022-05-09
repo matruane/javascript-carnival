470b525a9e9c1e250dc6cca495067f7252a17b33
Mat Ruane <mat@ruane.net.nz>
2022-05-09 14:31:16 +1200

    Implemented inflate()

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,28 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+const unicorns = document.querySelectorAll('.inflate-an-image')
 
-console.log("Inflate The Unicorn!")
+const inflate = (e) => {
+  // find the index of unicorn number
+  let idx = e.target.src.indexOf('unicorn-') + 'unicorn-'.length
+
+  // find the number of the unicorn clicked
+  let num
+
+  unicorns.forEach((unicorn, idx) => {
+    if (unicorn === e.target) {
+      num = idx + 1
+    }
+  })
+
+  if (e.target.src[idx] < 3) {
+    e.target.src = `./images/unicorn-${Number(e.target.src[idx]) + 1}.png`
+  } else {
+    alert(`Unicorn number ${num} says thank you!`)
+  }
+}
+
+unicorns.forEach((unicorn) => {
+  unicorn.onclick = inflate
+})
