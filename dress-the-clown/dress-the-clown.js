e4695940bb49de83795206bcbc41d1d07e50897e
Mat Ruane <mat@ruane.net.nz>
2022-05-10 08:46:56 +1200

    Head now changes in forwards and backwards direction using left/right arrow keys

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -6,19 +6,23 @@ const clownHead = document.querySelector('.dress-an-image.head')
 
 let headIndex = 0
 
-const changeClownHead = () => {
-  headIndex >= 5 ? (headIndex = 0) : headIndex++
+const changeClownHead = (direction) => {
+  if (direction === 'forward') {
+    headIndex >= 5 ? (headIndex = 0) : headIndex++
+  } else if (direction === 'backward') {
+    headIndex <= 0 ? (headIndex = 5) : headIndex--
+  }
 
   clownHead.src = `./images/head${headIndex}.png`
 }
 
-changeClownHead()
-
 document.addEventListener('keydown', (e) => {
   switch (e.code) {
+    case 'ArrowLeft':
+      changeClownHead('backward')
+      break
     case 'ArrowRight':
-      changeClownHead()
-      console.log('ArrowRight press')
+      changeClownHead('forward')
       break
   }
 })
