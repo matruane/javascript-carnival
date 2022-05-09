2d9f63acc3d07c94ee3e0d95fd10652d9d134989
Mat Ruane <mat@ruane.net.nz>
2022-05-10 09:14:08 +1200

    :sparkles: All clothes can be changed using the arrow keys

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -3,26 +3,50 @@
 // -    -   -   -   -  //
 
 const clownHead = document.querySelector('.dress-an-image.head')
+const clownBody = document.querySelector('.dress-an-image.body')
+const clownShoes = document.querySelector('.dress-an-image.shoes')
 
-let headIndex = 0
+let clothingTypeIndex = 0
+const clothingIndices = [3, 4, 4] // [0] = head, [1] = body, [2] = shoes
 
-const changeClownHead = (direction) => {
+const changeClothingType = (direction) => {
+  if (direction === 'down') {
+    clothingTypeIndex >= 2 ? (clothingTypeIndex = 0) : clothingTypeIndex++
+  } else if (direction == 'up') {
+    clothingTypeIndex <= 0 ? (clothingTypeIndex = 2) : clothingTypeIndex--
+  }
+}
+
+const changeClothes = (type, direction) => {
   if (direction === 'forward') {
-    headIndex >= 5 ? (headIndex = 0) : headIndex++
+    clothingIndices[type] >= 5
+      ? (clothingIndices[type] = 0)
+      : clothingIndices[type]++
   } else if (direction === 'backward') {
-    headIndex <= 0 ? (headIndex = 5) : headIndex--
+    clothingIndices[type] <= 0
+      ? (clothingIndices[type] = 5)
+      : clothingIndices[type]--
   }
 
-  clownHead.src = `./images/head${headIndex}.png`
+  clownHead.src = `./images/head${clothingIndices[0]}.png`
+  clownBody.src = `./images/body${clothingIndices[1]}.png`
+  clownShoes.src = `./images/shoes${clothingIndices[2]}.png`
 }
 
 document.addEventListener('keydown', (e) => {
+  e.preventDefault()
   switch (e.code) {
+    case 'ArrowUp':
+      changeClothingType('up')
+      break
+    case 'ArrowDown':
+      changeClothingType('down')
+      break
     case 'ArrowLeft':
-      changeClownHead('backward')
+      changeClothes(clothingTypeIndex, 'backward')
       break
     case 'ArrowRight':
-      changeClownHead('forward')
+      changeClothes(clothingTypeIndex, 'forward')
       break
   }
 })
